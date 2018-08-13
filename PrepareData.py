fe7a59c2763049241796c83afbe3b2cce4a22c53
Krisko <patrick.krisko@va1macwg099hv2t.newcorp.com>
2018-08-13 11:11:47 -0400

    Added PrepareData.py and moved around old data files to oldData/

--- PrepareData.py
+++ PrepareData.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 
-min_count = 764 # From stolem
+MIN = 764
 df = pd.read_csv("clean-peril-data.csv")
 df = df.drop_duplicates()
+# REMOVALS
 df = df.loc[df['COVERED_EVENT_CODE']!='OTHER',:]
+df = df.loc[df['COVERED_EVENT_CODE']!='NOTSATDEV',:]
+# COMBINE LOST / STOLEN
 df['COVERED_EVENT_CODE'] = np.where(df['COVERED_EVENT_CODE'].isin(['LOST', 'UNRECOV']), 'LOST/UNREC', df['COVERED_EVENT_CODE'])
-counters = {'LOST/UNREC': 0, 'STOLEN': 0, 'LQDDMG': 0, 'CRCKSRCN': 0, 'MLFUNC': 0}
+counters = {'LOST/UNREC': 0, 'STOLEN': 0, 'LQDDMG': 0, 'CRCKSCRN': 0, 'MLFUNC': 0}
+# FOR EACH ROW IN DF
+drop_idxs = []
 for idx, row in df.iterrows():
-    counters[df['COVERED_EVENT_CODE']] = counters[df['COVERED_EVENT_CODE']] + 1
-    # print(df['COVERED_EVENT_CODE'])
-print counters
+    counters[df.at[idx, 'COVERED_EVENT_CODE']] = counters[df.at[idx, 'COVERED_EVENT_CODE']] + 1
+    if counters[df.at[idx, 'COVERED_EVENT_CODE']] > MIN:
+        drop_idxs.append(idx)
+df = df.drop(drop_idxs)
 df.to_csv('ass.csv', index=False)
---
+++ SplitData.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv("ass.csv")
+df = df.drop_duplicates()
+
+df,df_test = train_test_split(df, test_size = 0.2, random_state = 42)
+df.to_csv('trainData.csv', index=False)
+df_test.to_csv('testData.csv', index=False)
