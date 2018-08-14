3d5378df9819bbca94ab50328fb1a72b8927f44b
Krisko <patrick.krisko@va1macwg099hv2t.newcorp.com>
2018-08-14 16:22:11 -0400

    prepping the food

--- FoodPrep.py
+++ FoodPrep.py
@@ -1,18 +1,37 @@
 from TransformData import *
 
 df = pd.read_csv('testData.csv')
+tries = pd.read_hdf('Stores/trie.h5')
 
 testing_objects = []
 
 for idx, row in df.iterrows():
     keywords = clean_words(df.at[idx, 'FAILURE_DESCRIPTIVE_TEXT'].split())
     testing_objects.append({
-        'peril': df.at[idx, 'COVERED_EVENT_CODE'],
-        'keywords': keywords
+        'real_peril': df.at[idx, 'COVERED_EVENT_CODE'],
+        'keywords': keywords,
+        'predicted_peril': [],
+        'true_positive': -1,
+        'false_positive': -1,
+        'num_pred_perils': -1
     })
 
 
-for pkt in testing_objects:
-    print pkt['peril'], pkt['keywords']
-    # print pkt['peril'], pkt['keywords':3]
-# print all_keywords_array
+def recurse_add_probs(entry, dictionary, index, max_len):
+    if index < max_len:
+        word = entry['keywords'][index]
+        if word in dictionary:
+            entry['predicted_peril'].append(dictionary[word]['_peril'])
+            recurse_add_probs(entry, dictionary[word], (index + 1), max_len)
+
+
+# print tries
+for entry in testing_objects:
+    keywords_len = len(entry['keywords'])
+    for idx in range(0, keywords_len):
+        recurse_add_probs(entry, tries, idx, keywords_len)
+
+
+
+
+
